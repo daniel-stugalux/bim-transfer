35784cc8f81fb8dd58979fb6e9c4b25482bb9c3a
Daniel Antunes <dantunes@stugalux.lu>
2021-03-05 12:07:40 +0100

    adding icons again

--- bim-website/src/components/Alert.js
+++ bim-website/src/components/Alert.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import Typography from '@material-ui/core/Typography';
+import SuccessImage from '../media/success.png';
+import FailImage from '../media/fail.png';
+
+const useStyles = makeStyles((theme) => ({
+    image: {
+        maxHeight: 200, 
+        marginTop: '20px',
+        position: 'relative',
+        left: '50%',
+        transform: 'translate(-50%, 0%)'
+    }
+}));
+
 
 
 const Alert = (props) => {
-    // const boom = props.severity === 'success' ? <img src={SuccessImage} alt="Success" /> : <img src={FailImage} alt="Fail" />
+    const classes = useStyles();
+
+    const boom = props.severity === 'error' ?  
+                                    <img src={FailImage} alt="Fail" className={classes.image}/> :
+                                    <img src={SuccessImage} alt="Success" className={classes.image}/>;
     var message = undefined;
     try {
         message = props.message.map(e => <Typography>{e}</Typography>);
@@ -19,10 +37,9 @@ const Alert = (props) => {
             onClose={props.handleClose}
             style={{marginTop: '50px'}}
         >
-            <MuiAlert severity={props.severity} onClose={props.handleClose}>
-                {
-                    message
-                }
+            <MuiAlert severity={props.severity} onClose={props.handleClose} style={{display: 'flex'}}>
+                <div>{message}</div>                
+                <div>{boom}</div>
             </MuiAlert>
         </Snackbar>
     )
